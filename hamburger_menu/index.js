b05e31720e2cd6e0d92d78416f82949bc6993a15
Ben1327 <ff.walker1327@gmail.com>
2020-11-05 17:20:51 +0900

    [Add] hamburger_menu 2

--- hamburger_menu/index.js
+++ hamburger_menu/index.js
@@ -1,6 +1,23 @@
 jQuery(function($){
-    $('.menu').on('click',function(){
-        $('.menu__line').toggleClass('active');
-        $('.gnav').fadeToggle();
-    });
+  var navFlg = false;
+  $('.menu').on('click',function(){
+    $('.menu__line').toggleClass('active');
+    $('.gnav').fadeToggle();
+    if(!navFlg){
+        $('.gnav__menu__item').each(function(i){
+            $(this).delay(i*300).animate({
+                'margin-left' : 0,
+                'opacity' : 1,
+            },800,'easeOutExpo');
+        });
+        navFlg = true;
+    }
+    else{
+        $('.gnav__menu__item').css({
+            'margin-left' : 100,
+            'opacity' : 0,
+        });
+        navFlg = false;
+    }
+  });
 });
